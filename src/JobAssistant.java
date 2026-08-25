38a3b2527f6d188e20b189db37f73f49307993fe
Stas Massalskiy <stmassalskiy@gmail.com>
2026-08-25 20:53:29 +0300

    Check limit Limit check stub

--- src/JobAssistant.java
+++ src/JobAssistant.java
@@ -1,5 +1,21 @@
 public class JobAssistant {
     public static void main(String[] args) {
-        System.out.println("JobAssistant started");
+
+        int dayClicks=0;
+        int dailyLimit=200;
+
+        Vacancy vacancy = new Vacancy("123456",
+                "Java","Yandex","Moscow",
+                "Mega Super", "http://");
+
+        System.out.println("Название вакансии: " + vacancy.getTitle());
+        System.out.println("Название компании: " + vacancy.getCompany());
+
+        if (dayClicks>=dailyLimit){
+            System.out.println("Лимит превышен");
+        }else {
+            System.out.println("Продолжаем отклики");
+        }
+
     }
 }
