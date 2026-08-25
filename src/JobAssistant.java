a954407b77bb8a87179eb66416a51ab340d47c4f
Stas Massalskiy <stmassalskiy@gmail.com>
2026-08-25 21:21:42 +0300

    Enter massive

--- src/JobAssistant.java
+++ src/JobAssistant.java
@@ -3,16 +3,19 @@ public class JobAssistant {
 
         int dayClicks=0;
         int dailyLimit=200;
+        boolean limitReached=dayClicks >= dailyLimit;
 
-        Vacancy vacancy = new Vacancy("123456",
-                "Java","Yandex","Moscow",
-                "Mega Super", "http://");
+        Vacancy[] vacancy ={
+                new Vacancy("123456", "Java","Yandex","Moscow", "Mega Super", "http://"),
+                new Vacancy("657896", "Ruby","Ozone","Moscow", "Mega", "http://"),
+                new Vacancy("367854", "C++","WB","Moscow", "Super", "http://")};
 
-        System.out.println("Название вакансии: " + vacancy.getTitle());
-        System.out.println("Название компании: " + vacancy.getCompany());
+        for(int i=0; i<vacancy.length;i++){
+            System.out.println(vacancy[i].getTitle());
+        }
 
-        if (dayClicks>=dailyLimit){
-            System.out.println("Лимит превышен");
+        if (limitReached){
+            System.out.println("Лимит достигнут");
         }else {
             System.out.println("Продолжаем отклики");
         }
